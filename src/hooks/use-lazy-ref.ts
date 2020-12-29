938d23d5bb6d2a2e976049d42574f1a1db98a489
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-12-29 14:39:47 +0100

    Tweak language

--- src/hooks/use-lazy-ref.ts
+++ src/hooks/use-lazy-ref.ts
@@ -2,11 +2,11 @@ import { MutableRefObject, useRef } from "react"
 import { Lazy } from "../types"
 import { applyLazyRef } from "../utils"
 
-export const useLazyRef = <T>(initialValue: Lazy<T>) => {
+export const useLazyRef = <T>(value: Lazy<T>) => {
   const ref: MutableRefObject<T | null> = useRef(null)
 
   if (ref.current === null) {
-    applyLazyRef(ref, initialValue)
+    applyLazyRef(ref, value)
   }
 
   return ref.current
--- src/hooks/use-previous-ref.ts
+++ src/hooks/use-previous-ref.ts
@@ -3,12 +3,12 @@ import { Lazy } from "../types"
 import { applyLazyRef } from "../utils"
 import { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect"
 
-export const usePreviousRef = <T>(initialValue: Lazy<T>) => {
-  const ref: MutableRefObject<T | null> = useRef(null)
+export const usePreviousRef = <T>(value: Lazy<T>) => {
+  const ref: MutableRefObject<T | undefined> = useRef()
 
   useIsomorphicLayoutEffect(() => {
-    applyLazyRef(ref, initialValue)
-  })
+    applyLazyRef(ref, value)
+  }, [value])
 
   return ref.current
 }
