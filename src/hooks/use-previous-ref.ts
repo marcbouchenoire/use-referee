14385953cf2f76b54c9033548de0b1757fe3af23
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2020-12-29 20:30:19 +0100

    Remove lazy value from usePreviousRef

--- src/hooks/use-previous-ref.ts
+++ src/hooks/use-previous-ref.ts
@@ -1,13 +1,11 @@
 import { MutableRefObject, useRef } from "react"
-import { Lazy } from "../types"
-import { applyLazyRef } from "../utils"
 import { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect"
 
-export const usePreviousRef = <T>(value: Lazy<T>) => {
+export const usePreviousRef = <T>(value: T) => {
   const ref: MutableRefObject<T | undefined> = useRef()
 
   useIsomorphicLayoutEffect(() => {
-    applyLazyRef(ref, value)
+    ref.current = value
   }, [value])
 
   return ref.current
