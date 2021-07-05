27c1bc107a9912e5ccb734514b1554565a8ddfa1
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-07-05 15:26:09 +0200

    Add eslint-plugin-unicorn

--- src/utils/apply-lazy-ref.ts
+++ src/utils/apply-lazy-ref.ts
@@ -3,11 +3,7 @@ import { isFunction } from "../guards"
 import { Lazy } from "../types"
 
 export function applyLazyRef<T>(ref: MutableRefObject<T>, value: Lazy<T>) {
-  if (isFunction(value)) {
-    ref.current = value()
-  } else {
-    ref.current = value
-  }
+  ref.current = isFunction(value) ? value() : value
 
   return ref
 }
